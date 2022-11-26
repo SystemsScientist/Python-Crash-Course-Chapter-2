75cbe6163afa981a8e92ec701a17b14ed1c6ac8a
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-26 06:48:53 -0600

    Adding exercise_02_01.py file

---
+++ exercise_02_01.py
@@ -0,0 +1,8 @@
+# exercise_02_01.py, Chapter 2, Python Crash Course
+# store a message in a variable, and then print that
+# message.
+
+secret_message = "This is a secret message."
+print(secret_message)
+
+
