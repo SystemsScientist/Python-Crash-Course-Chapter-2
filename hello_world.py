5bc9be0b25b8fb8bdc3ef8f9dca6b68b4156d382
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-26 06:43:14 -0600

    Adding hello_world.py file

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# hello_world.py, Chapter 02, Python Crash Course
+# program prints "Hello, Python World!"
+#
+# compile and execute program with the following
+# command-line scripts:
+# $ python3 hello_world.py
+
+message = "Hello, Python World!"
+print(message)
+
+message = "Hello, Python Crash Course World!"
+print(message)
+
+
