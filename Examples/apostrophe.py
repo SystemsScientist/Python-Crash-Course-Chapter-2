e18f0a1b4691dbbcca3b66a2b2a1a561d724663d
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-27 07:06:04 -0600

    Adding apostrophe.py file

---
+++ Examples/apostrophe.py
@@ -0,0 +1,7 @@
+# apostrophy.py, Chapter 02, Python Crash Course
+# program prints a message with an apostrophe
+
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+
