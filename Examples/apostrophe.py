d29f7499039bc3e9043a4d46f2885f52bb2765b4
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-27 07:07:33 -0600

    Updating apostrophy.py file

--- Examples/apostrophe.py
+++ Examples/apostrophe.py
@@ -1,4 +1,4 @@
-# apostrophy.py, Chapter 02, Python Crash Course
+# apostrophe.py, Chapter 02, Python Crash Course
 # program prints a message with an apostrophe
 
 message = "One of Python's strengths is its diverse community."
