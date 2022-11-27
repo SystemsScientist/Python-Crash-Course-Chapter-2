3bccdaefd0dd2e68f695246df31162d302f56e32
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-27 06:55:02 -0600

    Updating name.py file

--- Examples/name.py
+++ Examples/name.py
@@ -13,8 +13,9 @@ print(name.lower()) # name is the variable
 first_name = "ada"
 last_name = "lovelace"
 full_name = first_name + " " + last_name
-
 print(full_name)
-print("Hello, " + full_name.title() + "!")
+
+message = "Hello, " + full_name.title() + "!"
+print(message)
 
 
