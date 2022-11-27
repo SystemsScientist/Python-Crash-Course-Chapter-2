3ddf0c05cb84c478b0d5109f60b49335ed32e4f6
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-27 06:47:05 -0600

    Updating name.py file

--- Examples/name.py
+++ Examples/name.py
@@ -2,6 +2,12 @@
 # program prints "ada lovelace"
 
 name = "ada lovelace"
-print(name.title())
+print(name.title()) # name is the variable
+                    # title() is the method
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower()) # name is the variable
+                    # upper() is the method
 
 
