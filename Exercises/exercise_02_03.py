40c4b97d07d616644373fe9f121470ef6cc7bbcb
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-28 17:35:03 -0600

    Adding exercise_02_03.py file

---
+++ Exercises/exercise_02_03.py
@@ -0,0 +1,10 @@
+# exercise_02_03.py, Chapter 02, Python Crash Course
+# store a person's name in a variable, and print a
+# message to that person. Your message should be
+# simple, such as, "Hello Eric, would you like to
+# learn some Python today?"
+
+name = "Eric"
+print("Hello, " + name + ", would you like to learn some Python today?")
+
+
