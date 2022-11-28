eeaf686ebf23633ed97cd59a0f7ca9eeec50142e
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-28 17:41:39 -0600

    Adding exercise_02_04.py file

---
+++ Exercises/exercise_02_04.py
@@ -0,0 +1,13 @@
+# exercise_02_04.py, Chapter 02, Python Crash Course
+# 
+# Name Cases: store a person's name in a variable, and
+# then print that person's name in lowercase, uppercase,
+# and titlecase
+
+name = "benjamin franklin"
+
+print(name.lower()) # lowercase
+print(name.upper()) # uppercase
+print(name.title()) # titlecase
+
+
