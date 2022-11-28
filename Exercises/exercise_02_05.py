f7bc050be1bb5ec507fc53537feb2ad1425fee98
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-28 17:54:49 -0600

    Adding exercise_02_05.py file

---
+++ Exercises/exercise_02_05.py
@@ -0,0 +1,19 @@
+# exercise_02_05.py, Chapter 02, Python Crash Course
+#
+# Famous Quote: find a quote from a famous person you
+# admire. Print the quote and the name of its author.
+# Your output should look something like the following,
+# including the quotation mark:
+#
+#   Albert Einstein once said, "A person who never made
+#   a mistake never tried anything new."
+#
+# compile and execute this file with the following
+# command-line script:
+#
+# $ python3 exercise_02_05.py
+
+name = "david goggins"
+print(name.title() + " once said, \"On the other side of suffering is greatness!\"")
+
+
