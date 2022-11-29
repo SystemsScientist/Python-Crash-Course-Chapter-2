91bfac09a0a93dacb71ff689494ce4fa0993d5c7
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-28 18:03:18 -0600

    Adding exercise_02_06.py file

---
+++ Exercises/exercise_02_06.py
@@ -0,0 +1,18 @@
+# exercise_02_06.py, Chapter 02, Python Crash Course
+#
+# Famous Quote 2: repeat Exercise 2-5, but this time
+# store the famous person's name in a variable called
+# famous_peron. Then compose your message and store it
+# in a new variable called message. Print your message.
+#
+# compile and execute this file with the following
+# command-line script:
+#
+# $ python3 exercise_02_06.py
+
+famous_person = "david goggins"
+message = "\"On the other side suffering is greatness!\""
+
+print(famous_person.title() + " once said, " + message)
+
+
