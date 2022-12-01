a5c6913ed287b03ca6da9a8a16ba8821e9f04fb7
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-01 12:58:10 -0600

    Adding exercise_02_09.py file

---
+++ Exercises/exercise_02_09.py
@@ -0,0 +1,12 @@
+# exercise_02_09.py, Chapter 02, Python Crash Course
+#
+# Favorite Number: store your favorite number in a variable.
+# Then, using that variable, create a message that reveals
+# your favorite number. Print that message.
+
+favorite_number = 44
+message = "My favorite number is " + str(favorite_number) + "!"
+
+print(message)
+
+
