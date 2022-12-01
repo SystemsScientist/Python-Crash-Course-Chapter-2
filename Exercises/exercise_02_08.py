0cbd0c0af87ca59a26c82842e9a3a0a07582fd39
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-01 12:46:56 -0600

    Adding exercise_02_08.py

---
+++ Exercises/exercise_02_08.py
@@ -0,0 +1,19 @@
+# exercise_02_08.py, Chapter 02, Python Crash Course
+#
+# Number Eight: write addition, subtraction, multiplication
+# and division operations that each result in the number 8.
+# Be sure to enclose your operations in print statements to
+# see the results. You should create four lines that look
+# like this:
+#
+#   print(5 + 3)
+#
+# Your output should simply be four lines with the number 8
+# appearing once on each line.
+
+print(5 + 3)
+print(11 - 3)
+print(2 * 4)
+print(8 / 1)
+
+
