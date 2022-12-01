22dbd31b2052a93b3c7985a61b0301008fdae641
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-01 16:03:51 -0600

    Adding exercise_02_11.py file

---
+++ Exercises/exercise_02_11.py
@@ -0,0 +1,9 @@
+# exercise_02_11.py, Chapter 02, Python Crash Course
+#
+# Zen of Python: enter import this into a Python terminal
+# session and skim through the additional principles.
+
+
+import this
+
+
