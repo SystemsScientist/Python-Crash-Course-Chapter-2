f664810a823c5673ab6bfb3d6666f9c1abbf4afe
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-01 15:51:37 -0600

    Adding exercise_02_10.py file

---
+++ Exercises/exercise_02_10.py
@@ -0,0 +1,19 @@
+# exercise_02_10.py, Chapter 02, Python Crash Course
+#
+# Adding Comments: choose two of the programs you've
+# written, and ad at least one comment to each. If
+# you don't have anything specific to write because
+# your programs are too simple at this point, just 
+# add your name and the current date at the top of
+# each program file. Then write one sentence 
+# describing what the program does.
+
+message1 = "Since all of the other programs in this \n"
+message2 = "Chapter have comments, I will concatenate "
+message3 = "four variables together to make one long, \n"
+message4 = "very long sentence, which is too long."
+
+print(message1 + message2)
+print(message3 + message4)
+
+
